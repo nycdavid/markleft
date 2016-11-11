ab8c140eee7f0148453f093c2ffc17abac7c3e75
David Ko <david.ko@damncarousel.com>
2016-11-11 07:54:57 -0500

    Got newlines and the post request body working

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 * Built on Go 1.7+
 * Go server that receives a markdown string and returns a HTML string ready to be placed in a template.
 * Designed to be used with a front-end client that requires Markdown parsing as a visual aspect.
+
+# Handing a JSON `POST` request body
--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
-	"net/http"
-	"strings"
 	"text/template"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
-	"github.com/russross/blackfriday"
 )
 
 type Template struct {
@@ -20,24 +18,28 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
+type ReqBody struct {
+	Markdown string `json:"markdown"`
+}
+
 func main() {
 	t := &Template{
 		templates: template.Must(template.ParseGlob("public/views/*.html")),
 	}
 	e := echo.New()
 	e.SetRenderer(t)
-	e.Get("/", Hello)
+	e.Post("/", Hello)
 	e.Run(standard.New(":1323"))
 }
 
 func Hello(ctx echo.Context) error {
-	newStr := "* Foo \\n* Bar"
-	strings.Replace(newStr, "\\n", "\n", -1)
-	fmt.Println(newStr)
-	header := string(ctx.Request().Header().Get("X-Markdown"))
-	markdown := []byte(header)
-	output := string(blackfriday.MarkdownCommon(markdown))
-	return ctx.Render(http.StatusOK, "markdown-tmpl", output)
+	var rb ReqBody
+	d := json.NewDecoder(ctx.Request().Body())
+	d.Decode(&rb)
+	fmt.Println(rb.Markdown)
+	return nil
+	// output := string(blackfriday.MarkdownCommon(markdown))
+	// return ctx.Render(http.StatusOK, "markdown-tmpl", output)
 }
 
 // 1. e.SetRenderer expects an arg that is of type Renderer
