6b73590a570baaa2aceb382526f614cc513ffe90
David Ko <david.ko@damncarousel.com>
2016-11-13 21:55:36 -0500

    wip: markup test

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -49,4 +50,25 @@ func TestMarkdownKeyMissingReturnsError(t *testing.T) {
 	if jsonStr != errorMsg {
 		t.Errorf("Expected response to be %s, but got %s", errorMsg, jsonStr)
 	}
+}
+
+func TestValidMarkdownReturnsMarkup(t *testing.T) {
+	var jsonStr string
+	markdownJson := `{"markdown": "* Bar"}`
+	tmpl := &Template{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+	e := echo.New()
+	e.SetRenderer(tmpl)
+	req, _ := http.NewRequest(echo.POST, "/", strings.NewReader(markdownJson))
+	rec := httptest.NewRecorder()
+	c := e.NewContext(standard.NewRequest(req, e.Logger()), standard.NewResponse(rec, e.Logger()))
+	MarkdownHandler(c)
+	jsonBuf, _ := ioutil.ReadAll(rec.Body)
+	json.Unmarshal(jsonBuf, &jsonStr)
+
+	fmt.Println(jsonStr)
+	// if jsonStr != errorMsg {
+	// 	t.Errorf("Expected response to be %s, but got %s", errorMsg, jsonStr)
+	// }
 }
